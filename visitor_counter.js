9848533e47c80479de1546111baea7ab87321ba7
Gal Halevi <93094499+gal-halevi@users.noreply.github.com>
2025-02-18 21:51:37 +0200

    Add visitor counter
    apiUrl to invoke has to come from a config.json that is deployed with the frontend code

---
+++ visitor_counter.js
@@ -0,0 +1,45 @@
+const visitorCount = 'visitorCount'
+
+document.addEventListener('DOMContentLoaded', () => {
+    const counterElement = document.getElementById(visitorCount);
+    const cachedCount = localStorage.getItem(visitorCount);
+    if (cachedCount !== null) {
+        counterElement.textContent = cachedCount;
+    } else {
+        counterElement.textContent = 'Loading...';
+    }
+    updateCounter(counterElement);
+});
+
+async function updateCounter(counterElement) {
+    try {
+        config = await getConfig()
+        const response = await fetch(config.apiUrl, {
+            method: 'POST',
+            headers: {
+                'Accept': 'application/json',
+            },
+        });
+
+        if (!response.ok) {
+            throw new Error(`HTTP error: ${response.status}`);
+        }
+
+        const data = await response.json();
+        const newCount = data.visitorCount;
+
+        counterElement.textContent = newCount;
+        localStorage.setItem(visitorCount, newCount);
+    } catch (error) {
+        console.error("Error updating counter:", error);
+    }
+}
+
+async function getConfig() {
+    const configResponse = await fetch('config.json');
+    if (!configResponse.ok) {
+      throw new Error(`Failed to load config.json: ${configResponse.status}`);
+    }
+    const config = await configResponse.json();
+    return config
+}
