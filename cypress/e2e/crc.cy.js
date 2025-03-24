b01b87f6cd992ec2f84a8ce60159107804e06d77
Gal Halevi <93094499+gal-halevi@users.noreply.github.com>
2025-03-24 18:16:51 +0200

    Update with more smoke tests

--- cypress/e2e/crc.cy.js
+++ cypress/e2e/crc.cy.js
@@ -1,20 +1,51 @@
 describe('My CRC - Smoke Test', () => {
-    beforeEach(() => {
-      cy.visit('/'); // Adjust the path if necessary
-    });
-  
-    it('should have a visitor counter element', () => {
-      cy.get('#visitorCount').should('exist');
-    });
+  beforeEach(() => {
+    cy.visit('/src/index.html'); // Adjust the path if necessary
+  });
 
-    it('should update visitor count after API response', () => {
-        cy.intercept('POST', 'https://8w6pp5iid7.execute-api.us-east-1.amazonaws.com/default/updateVisitorCounter', {
-          statusCode: 200,
-          body: { visitorCount: 123 },
-        }).as('getVisitorCount');
-      
-        cy.wait('@getVisitorCount'); // Wait for the API call to complete
-      
-        cy.get('#visitorCount').should('have.text', '123');
+  it('should update visitor count after API response', () => {
+      cy.intercept('POST', 'https://vl974b3ut5.execute-api.us-east-1.amazonaws.com/default/visitorCounter', {
+        statusCode: 200,
+        body: { visitorCount: 123 },
+      }).as('getVisitorCount');
+    
+      cy.wait('@getVisitorCount'); // Wait for the API call to complete
+    
+      cy.get('#visitorCount').should('have.text', '123');
+  });
+
+  it("should increment visitor counter after refresh", () => {
+    cy.get("#visitorCount")
+      .should("be.visible")
+      .invoke("text")
+      .should("match", /^\d+$/) // Ensure it contains only digits
+      .then((initialCount) => {
+        initialCount = parseInt(initialCount, 10); // Convert to integer
+        cy.reload();
+
+        cy.wait(1000);
+        
+        cy.get("#visitorCount")
+          .should("be.visible")
+          .invoke("text")
+          .should("match", /^\d+$/) // Ensure new count is also a number
+          .then((newCount) => {
+            newCount = parseInt(newCount, 10);
+            expect(newCount).to.be.greaterThan(initialCount);
+          });
       });
   });
+
+  it("should handle error when an invalid payload is submitted", () => {
+
+    // Send an invalid payload directly to the API
+    cy.request({
+      method: "POST",
+      url: "https://vl974b3ut5.execute-api.us-east-1.amazonaws.com/default/visitorCounter", // Use relative URL if baseUrl is set
+      body: { invalidKey: "invalidValue" }, // Example of invalid payload
+      failOnStatusCode: false // Prevent Cypress from failing on non-2xx responses
+    }).then((response) => {
+      expect(response.status).to.be.equal(500); // Expecting a validation error
+    });
+  });
+});
