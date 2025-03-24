24b63b8f6bb6783300e4db98f46f9753c744dc56
Gal Halevi <93094499+gal-halevi@users.noreply.github.com>
2025-03-24 18:51:23 +0200

    Update test with using env vars

--- cypress/e2e/crc.cy.js
+++ cypress/e2e/crc.cy.js
@@ -1,10 +1,10 @@
 describe('My CRC - Smoke Test', () => {
   beforeEach(() => {
-    cy.visit('/src/index.html'); // Adjust the path if necessary
+    cy.visit('/src/html/index.html'); // Adjust the path if necessary
   });
 
   it('should update visitor count after API response', () => {
-      cy.intercept('POST', 'https://vl974b3ut5.execute-api.us-east-1.amazonaws.com/default/visitorCounter', {
+      cy.intercept('POST', Cypress.env("API_URL"), {
         statusCode: 200,
         body: { visitorCount: 123 },
       }).as('getVisitorCount');
@@ -41,11 +41,11 @@ describe('My CRC - Smoke Test', () => {
     // Send an invalid payload directly to the API
     cy.request({
       method: "POST",
-      url: "https://vl974b3ut5.execute-api.us-east-1.amazonaws.com/default/visitorCounter", // Use relative URL if baseUrl is set
-      body: { invalidKey: "invalidValue" }, // Example of invalid payload
-      failOnStatusCode: false // Prevent Cypress from failing on non-2xx responses
+      url: Cypress.env("API_URL"),
+      body: { invalidKey: "invalidValue" },
+      failOnStatusCode: false
     }).then((response) => {
-      expect(response.status).to.be.equal(500); // Expecting a validation error
+      expect(response.status).to.be.equal(500);
     });
   });
 });
